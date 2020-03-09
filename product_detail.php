92a25174edb3458ca56c2b20fda812c46dd9044f
SoftwaresBuilder <56379870+SoftwaresBuilder@users.noreply.github.com>
2020-03-09 11:40:17 +0500

    changing in chat module for product detail

--- product_detail.php
+++ product_detail.php
@@ -324,8 +324,8 @@ function fnAdd_bidd(){
         <?php
         $latitude = $product[0]['latitude'];
         $$longitude = $product[0]['longitude'];
-         include("gmap2.php");
-          ?>
+        include("gmap2.php");
+        ?>
         <div class="heading3"><?php echo $product[0]['title']; ?></div>
         <div><i class="fa fa-phone"></i>&nbsp;<?php echo $product[0]['location']; ?></div>
       </div>
@@ -333,10 +333,8 @@ function fnAdd_bidd(){
         <div class="row">
           <div class="col-2"><img src="images/user.jpg" class="user_icon"></div>
           <div class="col-10">
-
-            By <a href="vendor_profile.php?id=<?=enc_password($get_user_detail[0]['id']);?>">
-
-              <?php echo $get_user_detail[0]['name']; ?></a><br>
+            By
+            <a href="vendor_profile.php?id=<?=enc_password($get_user_detail[0]['id']);?>"><?php echo $get_user_detail[0]['name']; ?></a><br>
             <?php echo $product[0]['created_date']; ?> - Views <span class="bold">6</span></br>
             <?php if($user_online[0]['login']=='1')
             { echo '<h3 style="color:red;">online</h3>';} ?>
@@ -355,58 +353,8 @@ function fnAdd_bidd(){
       </div>
     </div>
 
-          <!-- here i will code -->
-
-
-
-    <div id="ChatMsg" class="ChatBar ui-block" style="width: 30%;display: none;">
-
-    <h3 class="light-text-0 ui-block-title" style="position: fixed;background-color: white;z-index: 999;width: inherit">
-             <span class="user-image round-img header-img">
-                        <img src="images/profile-placeholder.jpg" alt="DDDavid">
-              </span>
-        <a class="light-text-1 text-capitalize" href="/user/index">
-            david david            <i class="fa fa-angle-double-right"></i>
-        </a>
-        <a href="javascript:void(0)" class="closebtn" onclick="closeChat()">×</a>
-    </h3>
-
-    <div class="ui-block-content" style="padding: 0;margin-top: 64px;">
-
-        <!-- Chat Render Code Is here-->
-        <input type="hidden" name="check" id="check" value="1">
-
-        <div class="chat-box">
-            <div id="displayMsg" class=" chat-message-field p-3" style="overflow-y: scroll;height:570px;">
-                <div class="chatTwoTrwapper">
-                                    <div class="reciever">
-                                       
-                                    </div>
-                                    <div class="name">
-                                        
-                                        <small class="time"></small>
-                                    </div>
-
-                                </div>
-
-                            </div>
-            <div class="chat_footer">
-                <div class="row no-gutters">
-                    <div class="col-10">
-                        <textarea id="msgboxtext" class="chat_text_input form-control p-3" style="padding:10px 2px;" placeholder="Text Here!"></textarea>
-
-                    </div>
-                    <div class="col-2">
-                        <button type="button" onclick="sendmsg(11,3)" class="chat_text_send text-primary col btn bg-white btn-sm ">
-                            <i class="fa fa-send"></i>
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </div>
-</div>
+    
+    <?php include("product_chat.php");?>
 
 
           <div class="bidd-details-sec">
