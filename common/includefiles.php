e744cd6932b3ad7a4333e0b643f2a6b109e6ac24
SoftwaresBuilder <56379870+SoftwaresBuilder@users.noreply.github.com>
2020-03-09 13:49:00 +0500

    added translation function and files

--- common/includefiles.php
+++ common/includefiles.php
@@ -3,4 +3,10 @@ include("db.php");
 include("tables.php");
 include_once("functions.php");
 include("constants.php");
+
+if(isset($_SESSION['lang']) and $_SESSION['lang']=="th"){
+	include("lang_thai.php");
+} else {
+	include("lang_english.php");
+}
 ?>
