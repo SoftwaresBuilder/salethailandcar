3b5287eb555b63bd21fb49f45fd10e1dc1532d55
SoftwaresBuilder <56379870+SoftwaresBuilder@users.noreply.github.com>
2020-03-09 15:52:26 +0500

    remove translate duplicate function

--- common/functions.php
+++ common/functions.php
@@ -373,8 +373,5 @@ function splitlimit($string, $length = 50, $ellipsis = '...')
    if (strlen($string) > $length)return substr($string, 0, $length) . ' ' . $ellipsis;
    else return $string;
 }
-function translate($text){
-return $text;
-}
 
 ?>
