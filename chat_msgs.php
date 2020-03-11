afa23a887906901b49c6f4a626483593872db006
AdnanAnsari123 <adnanansarig902@gmail.com>
2020-03-11 19:10:23 +0500

    chat msg convesion

--- chat_msgs.php
+++ chat_msgs.php
@@ -1,7 +1,34 @@
 <?php
 $chat_msg = ($v['user_id']==$user_id)?'chat_msg1':'chat_msg2';
 $_SESSION['chat']['last_id'][$chat_id] = $v['id'];
+$msg_id=$v['id'];
 ?>
 <div class="chat_msg">
-    <div class="<?php echo $chat_msg;?>"><div class="small_text"><?php echo dates_duration($v['created_date']);?></div><?php echo $v['msg'];?></div>
+    <div id="div_<?php echo $msg_id;?>" class="<?php echo $chat_msg;?>"><div class="small_text"><?php echo dates_duration($v['created_date']);?></div><?php echo $v['msg'];?></div><a id="a_<?php echo $msg_id;?>" href="javascript:;" onclick="translateChat(<?php echo $msg_id;?>)">Translate</a>
+    <div id="listing_chat"></div>
 </div>
+<script type="text/javascript">
+	
+	function translateChat(msg_id) {
+		var cid=msg_id;
+       $("#a_"+cid).hide();
+
+		// alert(cid);return false;
+		if (cid>0) {
+		$.ajax({
+                    type: "GET",
+                    url: "ajaxphp.php",
+                    dataType :'html',
+                    data: {cid:cid,p:'getTranslate'},
+                    success: function(data){
+                          // $("#listing_chat").html(data);
+                          $("#div_"+cid).append(data);
+
+
+                    }
+                });
+		}
+		
+	}
+</script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
