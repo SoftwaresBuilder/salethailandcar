9108bec46b825fb30a4725f4352a5cdc54db220d
AdnanAnsari123 <adnanansarig902@gmail.com>
2020-03-11 19:19:57 +0500

    title updated

--- search_by_map.php
+++ search_by_map.php
@@ -19,15 +19,8 @@ if(count($products)>0){
 }
 $cat_id = dec_password($_GET['id']); 
 ?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>Hello, world!</title>
-<!-- <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script> -->
+
 <script src="jquery.3.2.1.min.js" type="text/javascript"></script>
 <script src="script.js" type="text/javascript"></script>
 <!-- <script src="bootstrap.min.js" type="text/javascript"></script> -->
@@ -49,9 +42,7 @@ $cat_id = dec_password($_GET['id']);
         padding: 0;
       }
     </style>
-    
-  </head>
-  <body>
+
     
 	<div class="container-fluid">
     <div class="row">
@@ -59,7 +50,7 @@ $cat_id = dec_password($_GET['id']);
     		  <div id="map"></div>
         </div>
         <div class="col-12 col-lg-4" style="overflow-y: auto;height: 600px;margin-bottom: 30px;">
-        	<h3>Listing <?php echo $category[0]['title']; ?></h3>
+        	<h3>Listing <?php echo $category[0]['title_'.$lang]; ?></h3>
         	<div id="listing_area"></div>
         </div>
     </div>
@@ -112,41 +103,9 @@ $cat_id = dec_password($_GET['id']);
                     }
                 });
       }
-		  // $('#listing_area').html('Ajax Call');
 	  }
-	  
-	  // var labels = ['test1','test2','test3'];
       var locations = [
-        <?php echo $lat_lng;?>
-		/**
-		,
-        {lat: -33.848588, lng: 151.209834},
-		
-		{lat: -32.718234, lng: 149.363181},
-        {lat: -32.727111, lng: 149.371124},
-        {lat: -32.848588, lng: 149.209834},
-		
-        {lat: -33.851702, lng: 151.216968},
-        {lat: -34.671264, lng: 150.863657},
-        {lat: -35.304724, lng: 148.662905},
-        {lat: -36.817685, lng: 175.699196},
-        {lat: -36.828611, lng: 175.790222},
-        {lat: -37.750000, lng: 145.116667},
-        {lat: -37.759859, lng: 145.128708},
-        {lat: -37.765015, lng: 145.133858},
-        {lat: -37.770104, lng: 145.143299},
-        {lat: -37.773700, lng: 145.145187},
-        {lat: -37.774785, lng: 145.137978},
-        {lat: -37.819616, lng: 144.968119},
-        {lat: -38.330766, lng: 144.695692},
-        {lat: -39.927193, lng: 175.053218},
-        {lat: -41.330162, lng: 174.865694},
-        {lat: -42.734358, lng: 147.439506},
-        {lat: -42.734358, lng: 147.501315},
-        {lat: -42.735258, lng: 147.438000},
-        {lat: -43.999792, lng: 170.463352}
-		/**/
-      ]
+        <?php echo $lat_lng;?>]
     </script>
     <script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js">
     </script>
@@ -161,8 +120,6 @@ $cat_id = dec_password($_GET['id']);
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-  </body>
-</html>
 <?php
 include("footer.php");
 ?>
